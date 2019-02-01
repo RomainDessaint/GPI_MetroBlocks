692b41cb053714e0da517e4a406f1367ad3db398
RomainDessaint <romaindessaint@gmail.com>
2019-02-01 16:04:24 +0100

    CORE MAP
    commentaires + modifs

--- src/core/Tile.java
+++ src/core/Tile.java
@@ -1,13 +1,19 @@
 package core;
 
 public class Tile {
-	private int coordinateX;
-	private int coordinateY;
+	private int coordinateX;		//Coordonnée X de la tuile dans la carte
+	private int coordinateY;		//Coordonnée Y de la tuile dans la carte
 	
 	public Tile(int coordinateX, int coordinateY) {
 		this.coordinateX = coordinateX;
 		this.coordinateY = coordinateY;
 	}
-	
-	
+
+	public int getCoordinateX() {
+		return coordinateX;
+	}
+
+	public int getCoordinateY() {
+		return coordinateY;
+	}
 }
