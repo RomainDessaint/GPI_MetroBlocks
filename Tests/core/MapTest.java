e7e84397581af2446c7ae424524e8a957ed6ca3b
Darine <Darine@DESKTOP-CG7TJ1L>
2019-02-01 16:16:21 +0100

    Map Test

--- Tests/core/MapTest.java
+++ Tests/core/MapTest.java
@@ -4,10 +4,15 @@ import junit.framework.TestCase;
 
 public class MapTest extends TestCase {
 
+	
+	
+	
 	public void testCreateBoard() {
 		fail("Not yet implemented");
 	}
 
+	
+	
 	public void testPrintBoard() {
 		fail("Not yet implemented");
 	}
